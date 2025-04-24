3669875e6eef530758b980ae881ef8dd49e6e3aa
Gianni Stephanie Carreira Honda <giannistephaniecarreirahonda@MacBook-Pro-de-Gianni.local>
2025-04-24 20:37:42 +0900

    mensagem do commit

---
+++ src/Componentes/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Tio from './imgs/tio.JPG'
+
+export default function Header(){
+   return(
+    <header>
+        <img src={Tio}/>
+        <h1>CFB Cursos</h1>
+    </header>
+   )
+}
